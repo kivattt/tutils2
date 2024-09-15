20dd7c0e5bf7f4411fac49784c894247ebe30785
kivattt <kivattt@gmail.com>
2024-09-15 06:01:27 +0200

    Re-order steps in compile.bash

--- compile.bash
+++ compile.bash
@@ -7,8 +7,8 @@ fi
 
 ldflags="-s -w"
 
-CGO_ENABLED=0 go build -ldflags="$ldflags" ./cmd/ls && mv ./ls ./bin/ls
 CGO_ENABLED=0 go build -ldflags="$ldflags" ./cmd/pwd && mv ./pwd ./bin/pwd
-CGO_ENABLED=0 go build -ldflags="$ldflags" ./cmd/xxd && mv ./xxd ./bin/xxd
+CGO_ENABLED=0 go build -ldflags="$ldflags" ./cmd/ls && mv ./ls ./bin/ls
 CGO_ENABLED=0 go build -ldflags="$ldflags" ./cmd/ascii && mv ./ascii ./bin/ascii
+CGO_ENABLED=0 go build -ldflags="$ldflags" ./cmd/xxd && mv ./xxd ./bin/xxd
 CGO_ENABLED=0 go build -ldflags="$ldflags" ./cmd/countchars && mv ./countchars ./bin/countchars
